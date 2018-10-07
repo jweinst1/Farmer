c2ca321d8fdc3ad82b405952fd524f8bfc74eec2
Joshua Weinstein <jweinstein@splunk.com>
2018-10-07 00:20:04 -0700

    take out old files

--- core/finders.c
+++
@@ -1,6 +0,0 @@
-#include "finders.h"
-
-int is_punc_char(int ch)
-{
-	return ch == ',' || ch == ';';
-}
--- core/finders.h
+++
@@ -1,6 +0,0 @@
-#ifndef CORE_FINDERS_H
-#define CORE_FINDERS_H
-
-extern int is_punc_char(int ch);
-
-#endif
